33ca7f9d0660fdbdb8616e6cf17c7c5125f92e2f
gangjian <gangjian@meizu.com>
2020-03-18 11:22:04 +0800

    first commit

---
+++ src/main/java/com/vbeesoft/streamserver/StreamserverApplication.java
@@ -0,0 +1,13 @@
+package com.vbeesoft.streamserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vbeesoft/streamserver/StreamserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vbeesoft.streamserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
